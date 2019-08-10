0de2ad2bfe9e1eb3ed7e37455f0618875cbb3cc8
Vasiliy Zorin <Vasiliy.Zorin@comindware.com>
2019-08-10 16:10:27 +0300

    add test_bot

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-const TelegramBot = require('node-telegram-bot-api');
+// const TelegramBot = require('node-telegram-bot-api');
+const Telegraf = require('telegraf');
 
 const TG_BOT_TOKEN = '863853752:AAGRZ0qi2kLPsZJOqEKpOnq5RDMTYgsbpqo';
 
-const bot = new TelegramBot(TG_BOT_TOKEN, { polling: true });
+// const bot = new TelegramBot(TG_BOT_TOKEN, { polling: true });
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `privet. You typed: ${msg.text}`);
-})
+// bot.on('message', msg => {
+//     bot.sendMessage(msg.chat.id, `Hi. You typed: ${msg.text}`);
+// });
+
+const notes = ['one', 'two', 'three'];
+
+
+
+
+// bot.onText(/startspam/, (msg, match) => {
+//     interval = setInterval(() => {
+//         bot.sendMessage(notes[i % notes.length])
+//     }, 3000);
+// });
+
+// bot.onText(/stopspam/, (msg, match) => clearInterval(interval));
+
+
+
+const bot = new Telegraf(TG_BOT_TOKEN);
+// let interval;
+// interval = setInterval(() => {
+//     ctx.telegram.sendMessage(notes[i % notes.length]);
+// }, 3000);
+// bot.command('startspam', (ctx) => {
+//     interval = setInterval(() => {
+//         ctx.telegram.sendMessage(notes[i % notes.length]);
+//     }, 3000);
+// });
+// bot.command('stopspam', () => clearInterval(interval));
+
+let interval;
+bot.command('go', (ctx) => {
+    ctx.reply('started');
+        let i =0;
+        interval = setInterval(() => {
+                ctx.reply(notes[i % notes.length]);
+                i++;
+            }, 3000);
+});
+
+bot.command('no', (ctx) => {
+    ctx.reply('stopped');
+    clearInterval(interval);
+    
+});
+
+
+bot.on('text', (ctx) => ctx.reply('Hello World'));
+bot.launch();
+
+
 
